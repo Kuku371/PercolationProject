ae505a5ad7448b51a587a720c93f686a929ac745
Kuku371 <keshavkarumbu0@gmail.com>
2026-09-18 13:41:19 -0700

    67

--- Percolation.java
+++ Percolation.java
@@ -1,8 +1,40 @@
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 
 public class Percolation {
+    int[][] grid;
+    public Percolation(int n){
+        this.grid = new int[n][n];
+        for (int i=0; i<n; i++){
+            for (int j=0; j<n; j++){
+                grid[i][j]=0;
+            }
+        }
+    }
+    public void open(int row, int col){
+        try {
+            this.grid[row-1][col-1] = 1;
+        } catch(Exception e) {
+            throw new IllegalArgumentException("Out of bounds");
+        }
+    }
+    public boolean isOpen(int row, int col){
+        try {
+            return (this.grid[row-1][col-1]==1);
+        } catch(Exception e) {
+            throw new IllegalArgumentException("Out of bounds");
+        }
+    }
+    public boolean isFull(int row, int col){
+        if (row == 1){
+            return true;
+        } else {
+    }
+    public boolean percolates(){
 
-
+    }
     public static void main(String[] args) {
 
     }
--- PercolationStats.java
+++ PercolationStats.java
@@ -3,6 +3,30 @@ import edu.princeton.cs.algs4.StdStats;
 
 public class PercolationStats {
 
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials){
+
+    }
+
+    // sample mean of percolation threshold
+    public double mean(){
+
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev(){
+
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo(){
+
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi(){
+
+    }
 
     public static void main(String[] args) {
 
